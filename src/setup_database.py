af563a72fb32b7a9c62a5fd95b49ec2623305b22
Elitsa Yotkova <eyotkova@gmail.com>
2025-08-24 00:25:53 +0300

    clean up

--- src/setup_database.py
+++ src/setup_database.py
@@ -1,6 +1,4 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import psycopg2
 
@@ -10,13 +8,6 @@ load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PW = os.getenv("POSTGRES_PW")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PW}@localhost:5432/{POSTGRES_DB}"
-)
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 # Migration execution
@@ -42,8 +33,3 @@ def run_migration():
 
 if __name__ == "__main__":
     run_migration()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
